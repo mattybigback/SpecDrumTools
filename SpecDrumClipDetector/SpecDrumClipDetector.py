35fe5104a7be63507bea7871ce39c809ff14674a
Matt Harrison <mattybigback@gmail.com>
2023-05-01 14:31:04 +0100

    Changed clip detector algorithm Added clip detector to tzx-builder

--- SpecDrumClipDetector/SpecDrumClipDetector.py
+++ SpecDrumClipDetector/SpecDrumClipDetector.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser
 from colorama import Fore, Style
 
 parser = ArgumentParser()
-parser.add_argument("-i", "--input", dest="fileName", help="Open specified file")
+parser.add_argument("fileName", help="Open specified file")
 args = parser.parse_args()
 
 fileName = args.fileName
@@ -44,7 +44,10 @@ def assignSamples():
     global group3Sample2 #2048 bytes
     global group3Sample3 #3072 bytes
     global group3Sample4 #3072 bytes
+    global padding
 
+
+    padding = np.int8([0] * 1024)
     group2Sample1 = np.fromfile(fileName, dtype='int8', count=3072, offset = byteOffset)
     byteOffset += 3072
     group2Sample2 = np.fromfile(fileName, dtype='int8', count=3072, offset = byteOffset)
@@ -52,14 +55,17 @@ def assignSamples():
     group2Sample3 = np.fromfile(fileName, dtype='int8', count=3072, offset = byteOffset)
     byteOffset += 3072
     group3Sample1 = np.fromfile(fileName, dtype='int8', count=2048, offset = byteOffset)
+    group3Sample1 = np.append(group3Sample1, padding)
     byteOffset += 2048
     group3Sample2 = np.fromfile(fileName, dtype='int8', count=2048, offset = byteOffset)
+    group3Sample2 = np.append(group3Sample2, padding)
     byteOffset += 2048
     group3Sample3 = np.fromfile(fileName, dtype='int8', count=3072, offset = byteOffset)
     byteOffset += 3072
     group3Sample4 = np.fromfile(fileName, dtype='int8', count=3072, offset = byteOffset)
     byteOffset += 3072
     group1Sample1 = np.fromfile(fileName, dtype='int8', count=2048, offset = byteOffset)
+    group1Sample1 = np.append(group1Sample1, padding)
     byteOffset += 2048
 
     
@@ -71,7 +77,7 @@ def clipDetect(list1, list2, list3, name1, name2, name3):
     list2 = list2.astype('int16')
     list3 = list3.astype('int16')
     #sum the first 2048 bytes of each sample and report any instances of clipping
-    for x in range(2048):
+    for x in range(3072):
         total = list1[x]+list2[x]+list3[x]
         if total >= 128 or total <= -129:
             print(Fore.RED + 'Clip @ ' + str(x) +' when adding ' + name1 + ', ' + name2 + ' and ' + name3 + ' ('+ str(total) +')')
@@ -82,7 +88,7 @@ def clipDetect(list1, list2, list3, name1, name2, name3):
 
 
 if fileName == None:
-    print('No input file specified. Please specify a file path using -i.')
+    print("No input file specified. Please specify a file")
     sys.exit()
 
 verifyFiles()
